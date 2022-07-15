28e122ad98065036e7559c5f2101affa1ee6a29b
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-07-15 17:48:35 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,32 +11,23 @@ source /usr/local/turbolab.it/bash-fx/bash-fx.sh
 ## bash-fx is ready
 
 sudo bash /usr/local/turbolab.it/bash-fx/setup/start.sh ${SCRIPT_NAME}
-
-## Symlink (globally-available zzfirewall command)
-if [ ! -f "/usr/local/bin/${SCRIPT_NAME}" ]; then
-  ln -s ${INSTALL_DIR}${SCRIPT_NAME}.sh /usr/local/bin/${SCRIPT_NAME}
-fi
-
-if [ ! -f "/usr/local/bin/${SCRIPT_NAME}-reset" ]; then
-  ln -s ${INSTALL_DIR}${SCRIPT_NAME}-reset.sh /usr/local/bin/${SCRIPT_NAME}-reset
-fi
-
-if [ "$(hostname)" = "zane-boraso" ] && [ ! -f "/usr/local/bin/${SCRIPT_NAME}-generate" ]; then
-  ln -s ${INSTALL_DIR}generators/generate-lists.sh /usr/local/bin/${SCRIPT_NAME}-generate
-fi
-
-if [ ! -f "/usr/local/bin/${SCRIPT_NAME}-whitelist-update" ]; then
-  ln -s ${INSTALL_DIR}whitelister/whitelister.sh /usr/local/bin/${SCRIPT_NAME}-whitelist-update
-fi
+fxLinkBin ${INSTALL_DIR}${SCRIPT_NAME}.sh
+fxLinkBin ${INSTALL_DIR}${SCRIPT_NAME}-reset.sh
+fxLinkBin ${INSTALL_DIR}whitelister/whitelister.sh ${SCRIPT_NAME}-whitelist-update
 
 ## Copy the cron job
 if [ ! -f "/etc/cron.d/zzfirewall" ]; then
   cp "${INSTALL_DIR}cron/cron" "/etc/cron.d/zzfirewall"
 fi
 
+if [ ! -f "/usr/local/bin/${SCRIPT_NAME}-whitelist-update" ]; then
+  ln -s ${INSTALL_DIR}whitelister/whitelister.sh /usr/local/bin/${SCRIPT_NAME}-whitelist-update
+fi
+
+## maintainer stuff
 if [ "$(hostname)" = "zane-boraso" ]; then
+  fxLinkBin ${INSTALL_DIR}generators/generate-lists.sh ${SCRIPT_NAME}-generate
   cp "${INSTALL_DIR}generators/cron-maintainer" "/etc/cron.d/zzfirewall_maintainer"
 fi
 
 sudo bash /usr/local/turbolab.it/bash-fx/setup/the-end.sh ${SCRIPT_NAME}
-
