3e700918c14a6c4007c70f12f43f25b62822cf55
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2026-03-05 10:29:54 +0100

    letsencrypt renewal-hooks

--- hooks/lets-encrypt-web-90-post.sh
+++ hooks/lets-encrypt-web-90-post.sh
@@ -4,7 +4,7 @@ echo ""
 SCRIPT_NAME=zzfirewall
 source "/usr/local/turbolab.it/bash-fx/bash-fx.sh"
 fxHeader "🪝 zzfirewall + certbot: 90-post hook"
-source "${SCRIPT_DIR}hooks/lets-encrypt-web.sh"
+source "${SCRIPT_DIR}lets-encrypt-web.sh"
 
 
 fxTitle "Remove temp. HTTP(s) from all rule..."
